91dd094539420580f50907b44fa1b13123860b05
vera <wanghemeng@viinet.com>
2019-05-12 14:36:21 +0800

    first commit

---
+++ src/i18n/index.js
@@ -0,0 +1,16 @@
+const LangEnUs = require('./lang/en-US')
+const LangZhCn = require('./lang/zh-CN')
+
+// 字典
+const DICT = {
+  'zh-CN': LangZhCn,
+  'en-US': LangEnUs
+}
+
+// 支持语言列表
+const SUPPORTED_LANGUAGES = Object.keys(DICT)
+
+// 默认语言
+const DEFAULT_LANGUAGE = 'zh-CN'
+
+module.exports = { DICT, SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE }
